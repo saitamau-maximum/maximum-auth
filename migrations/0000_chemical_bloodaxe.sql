721fbce0b52159f81f9bbcdbb7bb421508ec9848
sor4chi <sorakawa0218@gmail.com>
2023-12-21 13:43:35 +0900

    feat: steup drizzle

---
+++ migrations/0000_chemical_bloodaxe.sql
@@ -0,0 +1,67 @@
+CREATE TABLE `access_tokens` (
+	`id` text PRIMARY KEY NOT NULL,
+	`client_id` text NOT NULL,
+	`access_token` text NOT NULL,
+	`expires_at` integer NOT NULL,
+	FOREIGN KEY (`client_id`) REFERENCES `clients`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `authorization_codes` (
+	`id` text PRIMARY KEY NOT NULL,
+	`client_id` text NOT NULL,
+	`code` text NOT NULL,
+	`state` text NOT NULL,
+	FOREIGN KEY (`client_id`) REFERENCES `clients`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `clients` (
+	`id` text PRIMARY KEY NOT NULL,
+	`name` text NOT NULL,
+	`redirect_uri` text NOT NULL,
+	`hashed_secret` blob NOT NULL,
+	`salt` blob NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE `refresh_tokens` (
+	`id` text PRIMARY KEY NOT NULL,
+	`client_id` text NOT NULL,
+	`refresh_token` text NOT NULL,
+	`expires_at` integer NOT NULL,
+	FOREIGN KEY (`client_id`) REFERENCES `clients`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `scope_clients` (
+	`scope_id` text NOT NULL,
+	`client_id` text NOT NULL,
+	PRIMARY KEY(`client_id`, `scope_id`),
+	FOREIGN KEY (`scope_id`) REFERENCES `scopes`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`client_id`) REFERENCES `clients`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `scopes` (
+	`id` text PRIMARY KEY NOT NULL,
+	`slug` text NOT NULL,
+	`name` text NOT NULL,
+	`description` text NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE `user_clients` (
+	`user_id` text NOT NULL,
+	`client_id` text NOT NULL,
+	PRIMARY KEY(`client_id`, `user_id`),
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`client_id`) REFERENCES `clients`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `users` (
+	`id` text PRIMARY KEY NOT NULL,
+	`username` text NOT NULL,
+	`email` text NOT NULL,
+	`hashed_password` blob NOT NULL,
+	`salt` blob NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `client_id` ON `access_tokens` (`client_id`);--> statement-breakpoint
+CREATE UNIQUE INDEX `access_token` ON `access_tokens` (`access_token`);--> statement-breakpoint
+CREATE UNIQUE INDEX `client_id` ON `authorization_codes` (`client_id`);--> statement-breakpoint
+CREATE UNIQUE INDEX `code` ON `authorization_codes` (`code`);
---
+++ src/db/schema.ts
@@ -0,0 +1,156 @@
+import { relations } from "drizzle-orm";
+import {
+  sqliteTable,
+  text,
+  blob,
+  integer,
+  uniqueIndex,
+  primaryKey,
+} from "drizzle-orm/sqlite-core";
+
+export const scopes = sqliteTable("scopes", {
+  id: text("id").primaryKey().notNull(),
+  slug: text("slug").notNull(),
+  name: text("name").notNull(),
+  description: text("description").notNull(),
+});
+
+export const scopesRelations = relations(scopes, ({ many }) => ({
+  clients: many(clients),
+}));
+
+export const scopeClients = sqliteTable(
+  "scope_clients",
+  {
+    scopeId: text("scope_id")
+      .notNull()
+      .references(() => scopes.id),
+    clientId: text("client_id")
+      .notNull()
+      .references(() => clients.id),
+  },
+  (t) => ({
+    pk: primaryKey({
+      name: "scope_clients_pk",
+      columns: [t.scopeId, t.clientId],
+    }),
+  })
+);
+
+export const scopeClientsRelations = relations(scopeClients, ({ many }) => ({
+  scope: many(scopes),
+  client: many(clients),
+}));
+
+export const clients = sqliteTable("clients", {
+  id: text("id").primaryKey().notNull(),
+  name: text("name").notNull(),
+  redirect_uri: text("redirect_uri").notNull(),
+  hashed_secret: blob("hashed_secret").notNull(),
+  salt: blob("salt").notNull(),
+});
+
+export const clientsRelations = relations(clients, ({ many }) => ({
+  users: many(users),
+  userClients: many(userClients),
+  scopes: many(scopes),
+}));
+
+export const authorizationCodes = sqliteTable(
+  "authorization_codes",
+  {
+    id: text("id").primaryKey().notNull(),
+    clientId: text("client_id")
+      .notNull()
+      .references(() => clients.id),
+    code: text("code").notNull(),
+    state: text("state").notNull(),
+  },
+  (t) => ({
+    clientIdIdx: uniqueIndex("client_id").on(t.clientId),
+    codeIdx: uniqueIndex("code").on(t.code),
+  })
+);
+
+export const authorizationCodesRelations = relations(
+  authorizationCodes,
+  ({ many }) => ({
+    client: many(clients),
+  })
+);
+
+export const accessTokens = sqliteTable(
+  "access_tokens",
+  {
+    id: text("id").primaryKey().notNull(),
+    clientId: text("client_id")
+      .notNull()
+      .references(() => clients.id),
+    accessToken: text("access_token").notNull(),
+    expiresAt: integer("expires_at").notNull(),
+  },
+  (t) => ({
+    clientIdIdx: uniqueIndex("client_id").on(t.clientId),
+    accessTokenIdx: uniqueIndex("access_token").on(t.accessToken),
+  })
+);
+
+export type TAccessTokens = typeof accessTokens.$inferInsert;
+
+export const accessTokensRelations = relations(accessTokens, ({ many }) => ({
+  client: many(clients),
+}));
+
+export const refreshTokens = sqliteTable("refresh_tokens", {
+  id: text("id").primaryKey().notNull(),
+  clientId: text("client_id")
+    .notNull()
+    .references(() => clients.id),
+  refreshToken: text("refresh_token").notNull(),
+  expiresAt: integer("expires_at").notNull(),
+});
+
+export const refreshTokensRelations = relations(refreshTokens, ({ many }) => ({
+  client: many(clients),
+}));
+
+export const users = sqliteTable("users", {
+  id: text("id").primaryKey().notNull(),
+  username: text("username").notNull(),
+  email: text("email").notNull(),
+  hashed_password: blob("hashed_password").notNull(),
+  salt: blob("salt").notNull(),
+});
+
+export const usersRelations = relations(users, ({ many }) => ({
+  clients: many(clients),
+}));
+
+export const userClients = sqliteTable(
+  "user_clients",
+  {
+    userId: text("user_id")
+      .notNull()
+      .references(() => users.id),
+    clientId: text("client_id")
+      .notNull()
+      .references(() => clients.id),
+  },
+  (t) => ({
+    pk: primaryKey({
+      name: "user_clients_pk",
+      columns: [t.userId, t.clientId],
+    }),
+  })
+);
+
+export const userClientsRelations = relations(userClients, ({ many }) => ({
+  user: many(users),
+  client: many(clients),
+}));
+
+export type TClients = typeof clients.$inferInsert;
+export type TAuthorizationCodes = typeof authorizationCodes.$inferInsert;
+export type TRefreshTokens = typeof refreshTokens.$inferInsert;
+export type TUsers = typeof users.$inferInsert;
+export type TUserClients = typeof userClients.$inferInsert;

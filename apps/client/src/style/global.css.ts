f23e44f0b8476bd69681f491b01983c106697223
sor4chi <sorakawa0218@gmail.com>
2023-12-24 06:34:27 +0900

    feat: use noto sans (from google font)

--- apps/client/src/style/global.css.ts
+++ apps/client/src/style/global.css.ts
@@ -3,22 +3,22 @@ import { globalStyle } from "@vanilla-extract/css";
 import { colorVars } from "./contract.css";
 
 globalStyle("body", {
-  backgroundColor: colorVars.gray[2],
-  fontFamily:
-    'X, -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
-  margin: 0,
-  padding: 0,
-  color: colorVars.gray[12],
+	backgroundColor: colorVars.gray[2],
+	fontFamily:
+		'"Noto Sans", "Noto Sans JP", sans-serif',
+	margin: 0,
+	padding: 0,
+	color: colorVars.gray[12],
 
-  minHeight: "100dvh",
-  height: "100%",
+	minHeight: "100dvh",
+	height: "100%",
 });
 
 globalStyle("html", {
-  scrollPaddingTop: "50dvh",
-  scrollBehavior: "smooth",
+	scrollPaddingTop: "50dvh",
+	scrollBehavior: "smooth",
 });
 
 globalStyle("*", {
-  margin: 0,
+	margin: 0,
 });
